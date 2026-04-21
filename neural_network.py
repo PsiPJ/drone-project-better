053b88bffe318edceecf811c72b654b3659f5769
Jay H Duvvuri <razerxyzy@gmail.com>
2026-04-21 16:04:24 -0400

    This is latest upgraded version with basic mujoco setup

--- neural_network.py
+++ neural_network.py
@@ -35,4 +35,28 @@ except ValueError as e:
 
 policy = DroneController()  #This is creating an instance of the drone controller we just made
 
-#Sensor ID
+
+#Sensor ID retrieval, we dont got names in mujoco, everything is ID based, so we need to retreive the ID via this method
+gyro_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_SENSOR, "body_gyro")  #This is getting the ID for the gyro sensor
+acc_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_SENSOR, "body_linacc")     #This is getting the ID for the acceleration sensor, Linacc stands for linear acceleration
+quat_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_SENSOR, "body_quat")     #This is getting the ID for the quaternian sensor, which is basically the orientation of the drone
+
+
+
+#--- Mujoco Simulation Loop ----
+print("Starting simulation...")
+
+with mujoco.viewer.launch_passive(model, data) as viewer:
+    while viewer.is_running():
+        step_start = time.time()                          #All of this is bascially basic mujoco environment setup, easy peezy lemon sqeezy
+
+        gyro = data.sensordata[model.sensor_adr[gyro_id]:model.sensor_adr[gyro_id]+3]  #This is retreiving the gyro data from the sensor data, we have to specify the range because we have 3 values for the gyro
+        acc = data.sensordata[model.sensor_adr[acc_id] : model.sensor_adr[acc_id]+3]   #Same as above since we have 3 acceleration value
+        quat = data.sensordata[model.sensor_adr[quat_id] : model.sensor_adr[quat_id]+4]   #Naa tho here we need 4 hence the +4
+
+        #NOW lets make 1 big 10 element array combining 3 gyros, 3 accels, and 4 quats
+        state = np.concatenate([gyro, acc, quat])   #This is combining all the sensor data into one big array that we can feed into our neural network
+        state_tensor - torch.FloatTensor(state)   #Now we need to convert this numpy array into a torch tensor so we can feed it into our neural network.  Pytorch refuses to read numpy arrays.
+        #BOOO PYTORCH, we demand respect for NUMPY people. LOL
+
+        
